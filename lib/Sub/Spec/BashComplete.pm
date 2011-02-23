f9adbf11f334c8a658ba463fd6456f2c716d634d
Steven Haryanto <stevenharyanto@gmail.com>
2011-02-23 03:08:38 +0000

    Import of SHARYANTO/Sub-Spec-BashComplete-0.12 from CPAN.
    gitpan-cpan-distribution: Sub-Spec-BashComplete
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         SHARYANTO/Sub-Spec-BashComplete-0.12.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sub/Spec/BashComplete.pm
+++ lib/Sub/Spec/BashComplete.pm
@@ -1,6 +1,6 @@
 package Sub::Spec::BashComplete;
 BEGIN {
-  $Sub::Spec::BashComplete::VERSION = '0.11';
+  $Sub::Spec::BashComplete::VERSION = '0.12';
 }
 # ABSTRACT: Provide bash completion for Sub::Spec::CmdLine programs
 
@@ -401,7 +401,7 @@ Sub::Spec::BashComplete - Provide bash completion for Sub::Spec::CmdLine program
 
 =head1 VERSION
 
-version 0.11
+version 0.12
 
 =head1 SYNOPSIS
 
